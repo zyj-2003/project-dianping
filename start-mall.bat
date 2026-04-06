16f942f0ec1bc0f29992f5033295c695c077c0c7
zyj-2003 <2786466348@qq.com>
2026-04-06 19:27:38 +0800

    first commit

---
+++ start-mall.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "%~dp0newbee-mall-master"
+java -jar target\newbee-mall-1.0.0-SNAPSHOT.jar
+pause
---
+++ start-mall.ps1
@@ -0,0 +1,2 @@
+cd "$PSScriptRoot\newbee-mall-master"
+java -jar target\newbee-mall-1.0.0-SNAPSHOT.jar
